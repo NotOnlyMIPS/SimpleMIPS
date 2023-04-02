045ef5ca38f14ad96104d73369073a3bb5c4979e
周上行Ryer <melaiiryer@gmail.com>
2023-04-02 21:05:26 +0800

    fix: all conflicts in the merge process
    1. remove weakly-relevant files
    2. move cache & all func testsuit

--- test/loongson/tb_cpu_top.v
+++ test/loongson/tb_cpu_top.v
@@ -32,6 +32,7 @@ THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ------------------------------------------------------------------------------*/
 `timescale 1ns / 1ps
 
+//! FIXME: trace file may need to be changed when running different tests
 `define TRACE_REF_FILE "../../../../../../../../share/cpu132_gettrace/golden_trace.txt"
 `define CONFREG_NUM_REG      soc_lite.u_confreg.num_data
 `define CONFREG_OPEN_TRACE   soc_lite.u_confreg.open_trace
