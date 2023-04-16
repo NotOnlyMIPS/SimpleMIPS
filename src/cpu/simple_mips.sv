c8b4231a75e1ec0869e0976781f81b0564e81f59
周上行Ryer <melaiiryer@gmail.com>
2023-04-16 17:25:21 +0800

    revert: 9b4c769d^..d8038e82
    This reverts commits 9b4c769d6eca5b97d093ded484fa6ef7d925799e1,
    cbb7b27f62ad80afeed1652e0717004a97291d991,
    d8038e829369a93ca60a0212ce6bde29becf50da1.

--- src/cpu/simple_mips.sv
+++ src/cpu/simple_mips.sv
@@ -1,13 +1,12 @@
-`include "../common.svh"
 `include "cpu.svh"
 
-module SimpleMIPS (
+module SimpleMIPS(
     input logic clk,
-    input logic rstn,
-    //! exception
+    input logic resetn,
+    // ex
     input logic [5:0] ext_int,
-    //! AXI
-    //! AXI/AR read address
+    // axi
+    //ar
     output [3 :0]   arid   ,
     output virt_t   araddr ,
     output [7 :0]   arlen  ,
@@ -18,14 +17,14 @@ module SimpleMIPS (
     output [2 :0]   arprot ,
     output          arvalid,
     input           arready,
-    //! AXI/R read data
+    //r
     input  [3 :0]   rid    ,
     input  uint32_t rdata  ,
     input  [1 :0]   rresp  ,
     input           rlast  ,
     input           rvalid ,
     output          rready ,
-    //! AXI/AW write address
+    //aw
     output [3 :0]   awid   ,
     output virt_t   awaddr ,
     output [7 :0]   awlen  ,
@@ -36,27 +35,26 @@ module SimpleMIPS (
     output [2 :0]   awprot ,
     output          awvalid,
     input           awready,
-    //! AXI/W write data
+    //w
     output [3 :0]   wid    ,
     output uint32_t wdata  ,
     output [3 :0]   wstrb  ,
     output          wlast  ,
     output          wvalid ,
     input           wready ,
-    //! AXI/B write response
+    //b
     input  [3 :0]   bid    ,
     input  [1 :0]   bresp  ,
     input           bvalid ,
     output          bready ,
-    //! debug interface for trace
+    // trace debug interface
     output [31:0] debug_wb_pc,
     output [ 3:0] debug_wb_rf_wen,
     output [ 4:0] debug_wb_rf_wnum,
     output [31:0] debug_wb_rf_wdata
 );
-
 reg reset;
-always @(posedge clk) reset <= ~rstn;
+always @(posedge clk) reset <= ~resetn;
 
 // CPU_ICache
 CPU_ICache_Interface CPU_ICache();
@@ -95,57 +93,17 @@ logic [ 31:0]   uncache_wr_data;
 logic           uncache_wr_rdy;
 logic           uncache_wr_bvalid;
 
-//! AXI
-axi_t axi;
-
-assign axi.arid         = arid;
-assign axi.req.araddr   = araddr;
-assign axi.req.arlen    = arlen;
-assign axi.req.arsize   = arsize;
-assign axi.req.arburst  = arburst;
-assign axi.req.arlock   = arlock;
-assign axi.req.arcache  = arcache;
-assign axi.req.arprot   = arprot;
-assign axi.req.arvalid  = arvalid;
-assign axi.resp.arready = arready;
-assign axi.rid          = rid;
-assign axi.req.rready   = rready;
-assign axi.resp.rdata   = rdata;
-assign axi.resp.rresp   = rresp;
-assign axi.resp.rlast   = rlast;
-assign axi.resp.rvalid  = rvalid;
-assign axi.awid         = awid;
-assign axi.req.awaddr   = awaddr;
-assign axi.req.awlen    = awlen;
-assign axi.req.awsize   = awsize;
-assign axi.req.awburst  = awburst;
-assign axi.req.awlock   = awlock;
-assign axi.req.awcache  = awcache;
-assign axi.req.awprot   = awprot;
-assign axi.req.awvalid  = awvalid;
-assign axi.resp.awready = awready;
-assign axi.wid          = wid;
-assign axi.req.wdata    = wdata;
-assign axi.req.wstrb    = wstrb;
-assign axi.req.wlast    = wlast;
-assign axi.req.wvalid   = wvalid;
-assign axi.resp.wready  = wready;
-assign axi.bid          = bid;
-assign axi.req.bready   = bready;
-assign axi.resp.bresp   = bresp;
-assign axi.resp.bvalid  = bvalid;
-
-cpu_core u_cpu_core (
-    //! common
+cpu_core u_cpu_core(
     .clk,
     .reset,
-    //! exception
+
     .ext_int,
-    //! ICache
+
+    // ICache
     .IBus             (CPU_ICache.CPU   ),
-    //! data_sram
+    // data_sram
     .DBus             (CPU_DCache.CPU   ),
-    //! debug interface
+    //debug interface
     .debug_wb_pc      (debug_wb_pc      ),
     .debug_wb_rf_wen  (debug_wb_rf_wen  ),
     .debug_wb_rf_wnum (debug_wb_rf_wnum ),
@@ -198,8 +156,83 @@ dcache u_dcache(
     .uwr_bvalid    (uncache_wr_bvalid)
 );
 
-cpu_axi_interface u_cpu_axi_interface (
-    .*
+cpu_axi_interface u_cpu_axi_interface(
+    .clk,
+    .reset,
+    // ICache
+    .icache_uncache    ,
+    .icache_req        ,
+    .icache_addr       ,
+    .icache_addr_ready ,
+    .icache_data_ready ,
+    .icache_rdata      ,
+    // DCache
+    .dcache_rd_req,
+    .dcache_rd_addr,
+    .dcache_rd_rdy,
+    .dcache_ret_valid,
+    .dcache_ret_data,
+    .dcache_wr_req,
+    .dcache_wr_addr,
+    .dcache_wr_data,
+    .dcache_wr_rdy,
+    .dcache_wr_bvalid,
+    // UnCache
+    .uncache_rd_req,
+    .uncache_rd_size,//
+    .uncache_rd_addr,
+    .uncache_rd_rdy,
+    .uncache_ret_valid,
+    .uncache_ret_data,
+    .uncache_wr_req,
+    .uncache_wr_size,//
+    .uncache_wr_addr,
+    .uncache_wr_wstrb,//
+    .uncache_wr_data,
+    .uncache_wr_rdy,
+    .uncache_wr_bvalid,
+    // axi
+    // ar
+    .arid   ,
+    .araddr ,
+    .arlen  ,
+    .arsize ,
+    .arburst,
+    .arlock ,
+    .arcache,
+    .arprot ,
+    .arvalid,
+    .arready,
+    // r
+    .rid   ,
+    .rdata ,
+    .rresp ,
+    .rlast ,
+    .rvalid,
+    .rready,
+    // aw
+    .awid   ,
+    .awaddr ,
+    .awlen  ,
+    .awsize ,
+    .awburst,
+    .awlock ,
+    .awcache,
+    .awprot ,
+    .awvalid,
+    .awready,
+    // w
+    .wid   ,
+    .wdata ,
+    .wstrb ,
+    .wlast ,
+    .wvalid,
+    .wready,
+    // b
+    .bid   ,
+    .bresp ,
+    .bvalid,
+    .bready
 );
 
 endmodule
